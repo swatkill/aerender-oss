2e268413bfa0136a11049d0608e9617ab726406a
Robin <252505244@qq.com>
2023-05-30 17:29:47 +0800

    fix

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const OSS = require('ali-oss');
 
-const putOSS = async(region,accessKeyId,accessKeySecret,object,localFile,logger) =>{
+const putOSS = async(region,accessKeyId,accessKeySecret,bucket,object,localFile,logger) =>{
     let client = new OSS({
         region: region,
         accessKeyId: accessKeyId,// 阿里云获取
-        accessKeySecret: accessKeySecret // 阿里云获取
+        accessKeySecret: accessKeySecret, // 阿里云获取
+        bucket: bucket // 阿里云获取
     });
     try {
         // 'object'填写上传至OSS的object名称,即不包括Bucket名称在内的Object的完整路径。
@@ -28,7 +29,7 @@ const run = async (job, settings, action, type,logger) => {
     const {logger} = settings;
     const {
         input,
-        params: { region,accessKeyId,accessKeySecret,object },
+        params: { region,accessKeyId,accessKeySecret,bucket,object },
     } = action;
     try {
         let finalInput = input ?? job.output;
@@ -39,7 +40,7 @@ const run = async (job, settings, action, type,logger) => {
             `[aeworker-oss] uploading to presigned-url: ${url}`
         );
 
-        await putOSS(region,accessKeyId,accessKeySecret,object,finalInput, logger);
+        await putOSS(region,accessKeyId,accessKeySecret,bucket,object,finalInput, logger);
     } catch (error) {
         logger.log(
             `[aeworker-oss] failed uploading to presigned-url: ${url} \n ${error?.message}`
