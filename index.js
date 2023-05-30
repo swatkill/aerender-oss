ea06d5fb6ada81659b8594d734738d4bfc6de4fb
swatkill <252505244@qq.com>
2023-05-30 14:48:02 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const OSS = require('ali-oss');
+
+const putOSS = async(region,accessKeyId,accessKeySecret,object,localFile,logger) =>{
+    let client = new OSS({
+        region: region,
+        accessKeyId: accessKeyId,// 阿里云获取
+        accessKeySecret: accessKeySecret // 阿里云获取
+    });
+    try {
+        // 'object'填写上传至OSS的object名称,即不包括Bucket名称在内的Object的完整路径。
+        // 'localfile'填写上传至OSS的本地文件完整路径。
+        let r1 = await client.put(object,localFile);
+        logger.log('put success: %j', r1);
+        let r2 = await client.get(object);
+        logger.log('get success: %j', r2);
+    } catch(e) {
+        logger.log('error: %j', e);
+    }
+}
+
+const run = async (job, settings, action, type,logger) => {
+    if (type != "postrender") {
+        throw new Error(
+            `[nexrender-action-upload-s3-presigned] action can be only run in postrender mode, you provided: ${type}.`
+        );
+    }
+    const {logger} = settings;
+    const {
+        input,
+        params: { region,accessKeyId,accessKeySecret,object },
+    } = action;
+    try {
+        let finalInput = input ?? job.output;
+        if (!path.isAbsolute(finalInput))
+            finalInput = path.join(job.workpath, finalInput);
+
+        logger.log(
+            `[aeworker-oss] uploading to presigned-url: ${url}`
+        );
+
+        await putOSS(region,accessKeyId,accessKeySecret,object,finalInput, logger);
+    } catch (error) {
+        logger.log(
+            `[aeworker-oss] failed uploading to presigned-url: ${url} \n ${error?.message}`
+        );
+        throw error;
+    }
+};
+
+module.exports = run;
